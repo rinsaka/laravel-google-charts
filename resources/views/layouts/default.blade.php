2a6c4cdc0fb18899a75d2ce81a8e60611ead25b3
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-07-25 16:03:31 +0900

    折れ線グラフ

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -20,28 +20,23 @@
 
         // Create the data table.
         var data = google.visualization.arrayToDataTable([
-                ['City', '2010 Population',],
-                ['New York City, NY', 8175000],
-                ['Los Angeles, CA', 3792000],
-                ['Chicago, IL', 2695000],
-                ['Houston, TX', 2099000],
-                ['Philadelphia, PA', 1526000]
-              ]);
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]);
 
         // Set chart options
         var options = {
-                title: 'Population of Largest U.S. Cities',
-                chartArea: {width: '50%'},
-                hAxis: {
-                  title: 'Total Population',
-                  minValue: 0
-                },
-                vAxis: {
-                  title: 'City'
-                }
-              };
+          title: 'Company Performance',
+          // curveType: 'function',
+          legend: { position: 'bottom' },
+          width:800,
+          height:500
+        };
         // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
     </script>
