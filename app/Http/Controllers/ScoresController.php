236b6f26d941e2a86567f7ebdae3b83a5a17797a
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-07-25 15:41:06 +0900

    route

--- app/Http/Controllers/ScoresController.php
+++ app/Http/Controllers/ScoresController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Score;
 
 class ScoresController extends Controller
 {
-    //
+  public function index()
+  {
+    $scores = Score::get();
+    dd($scores);
+  }
 }
