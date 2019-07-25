13732ad6244f8619285f5ee73f9be36cca9fc284
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-07-25 15:58:26 +0900

    viewのレイアウト化

--- resources/views/scores/index.blade.php
+++ resources/views/scores/index.blade.php
@@ -1,60 +1,15 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>スコア一覧</title>
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
+@extends('layouts.default')
 
-      // Load the Visualization API and the corechart package.
-      google.charts.load('current', {'packages':['corechart','bar'], 'language': 'ja'});
+@section('title', 'スコア一覧')
 
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
+@section('content')
 
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
+<ul>
+  @foreach ($scores as $score)
+    <li>{{ $score->created_at }} : {{ $score->user_id }} : {{ $score->score }}</li>
+  @endforeach
+</ul>
+<h2>グラフ</h2>
+<div id="chart_div"></div>
 
-        // Create the data table.
-        var data = google.visualization.arrayToDataTable([
-                ['City', '2010 Population',],
-                ['New York City, NY', 8175000],
-                ['Los Angeles, CA', 3792000],
-                ['Chicago, IL', 2695000],
-                ['Houston, TX', 2099000],
-                ['Philadelphia, PA', 1526000]
-              ]);
-
-        // Set chart options
-        var options = {
-                title: 'Population of Largest U.S. Cities',
-                chartArea: {width: '50%'},
-                hAxis: {
-                  title: 'Total Population',
-                  minValue: 0
-                },
-                vAxis: {
-                  title: 'City'
-                }
-              };
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-    </script>
-
-</head>
-<body>
-    <h1>スコア一覧</h1>
-    <ul>
-      @foreach ($scores as $score)
-        <li>{{ $score->created_at }} : {{ $score->user_id }} : {{ $score->score }}</li>
-      @endforeach
-    </ul>
-    <h2>グラフ</h2>
-    <div id="chart_div"></div>
-</body>
-</html>
+@endsection
